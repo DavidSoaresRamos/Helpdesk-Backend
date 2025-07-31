713d9f5747dbaf1d9a642b82d1fe033a9a07920e
DavidRamos <clashdavid527@gmail.com>
2025-07-31 00:58:39 -0300

    Delete helpdesk directory

--- helpdesk/src/main/java/com/yuziio/helpdesk/repository/ChamadosRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.yuziio.helpdesk.repository;
-
-import com.yuziio.helpdesk.model.Ticket;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChamadosRepositorio extends MongoRepository<Ticket, String> {
-}
